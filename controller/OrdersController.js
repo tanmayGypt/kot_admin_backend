826e29593cf50bdcba6295967a051568faf78cf8
Rakesh <krakesh7788@gmail.com>
2024-05-23 01:55:28 +0530

    added fetch room by encoded, rectify routes

--- controller/OrdersController.js
+++ controller/OrdersController.js
@@ -1,8 +1,8 @@
-const { where } = require("sequelize");
-const db = require("../models");
-const { FetchRoomById } = require("./RoomsContoller");
+const { where } = require("sequelize")
+const db = require("../models")
+const { FetchRoomById } = require("./RoomsContoller")
 
-const Order = db.Orders;
+const Order = db.Orders
 
 const AddNewOrder = async (
   OrderId,
@@ -15,7 +15,7 @@ const AddNewOrder = async (
   OrderStatus
 ) => {
   try {
-    let Room = FetchRoomById(RoomId);
+    let Room = FetchRoomById(RoomId)
     if (Room) {
       let result = await Order.create({
         OrderId,
@@ -26,17 +26,17 @@ const AddNewOrder = async (
         CreatedAt,
         Payment_Mode,
         OrderStatus,
-      });
-      console.log("Order created successfully:", result);
-      return result;
+      })
+      console.log("Order created successfully:", result)
+      return result
     } else {
-      return null;
+      return null
     }
   } catch (error) {
-    console.error("Error creating order:", error);
-    throw error; // Ensure the error is propagated
+    console.error("Error creating order:", error)
+    throw error // Ensure the error is propagated
   }
-};
+}
 
 const UpdateOrder = async (OrderId, Status, Payment_Mode, OrderStatus) => {
   try {
@@ -49,65 +49,66 @@ const UpdateOrder = async (OrderId, Status, Payment_Mode, OrderStatus) => {
       {
         where: { OrderId },
       }
-    );
+    )
 
     if (result[0] === 0) {
-      console.log(`No order found with OrderId: ${OrderId}`);
-      return null;
+      console.log(`No order found with OrderId: ${OrderId}`)
+      return null
     }
 
-    console.log("Order updated successfully:", result);
-    return result;
+    console.log("Order updated successfully:", result)
+    return result
   } catch (error) {
-    console.error("Error updating order:", error);
-    throw error; // Ensure the error is propagated
+    console.error("Error updating order:", error)
+    throw error // Ensure the error is propagated
   }
-};
+}
 
 const FetchAllOrders = async () => {
   try {
-    let response = await Order.findAll({});
-    console.log("Orders fetched successfully:", response);
-    return response;
+    let response = await Order.findAll({})
+    console.log("Orders fetched successfully:", response)
+    return response
   } catch (error) {
-    console.error("Error fetching orders:", error);
-    throw error; // Ensure the error is propagated
+    console.error("Error fetching orders:", error)
+    throw error // Ensure the error is propagated
   }
-};
+}
 const FetchOrderById = async (OrderId) => {
   try {
     let response = await Order.findOne({
       where: {
         OrderId,
       },
-    });
-    console.log("Order fetched successfully:", response);
-    return response;
+    })
+    console.log("Order fetched successfully:", response)
+    return response
   } catch (error) {
-    console.error("Error fetching orders:", error);
-    throw error; // Ensure the error is propagated
+    console.error("Error fetching orders:", error)
+    throw error // Ensure the error is propagated
   }
-};
+}
 
-const FetchOrderByCustomerId = async (CustomerId) => {
+const FetchUnbilledOrder = async (CustomerId) => {
   try {
-    let response = await Order.findOne({
+    let response = await Order.findAll({
       where: {
         CustomerId,
         isPaid: false,
       },
-    });
-    console.log("Order fetched successfully:", response);
-    return response;
+    })
+
+    console.log("Order fetched successfully:", response)
+    return response
   } catch (error) {
-    console.error("Error fetching orders:", error);
-    throw error; // Ensure the error is propagated
+    console.error("Error fetching orders:", error)
+    throw error // Ensure the error is propagated
   }
-};
+}
 module.exports = {
   AddNewOrder,
   UpdateOrder,
   FetchAllOrders,
   FetchOrderById,
-  FetchOrderByCustomerId,
-};
+  FetchUnbilledOrder,
+}
