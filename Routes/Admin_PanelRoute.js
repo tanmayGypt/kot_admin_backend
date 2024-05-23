9af566fb9d9ac89cfd4bfe1b6544f74eefeefb0e
tanmayGypt <tanmay631@gmail.com>
2024-05-24 01:55:46 +0530

    fixes

--- Routes/Admin_PanelRoute.js
+++ Routes/Admin_PanelRoute.js
@@ -1,69 +1,69 @@
-const TokenGenerator = require("../TokenGenerator")
-const auth = require("../auth")
+const TokenGenerator = require("../TokenGenerator");
+const auth = require("../auth");
 const {
   AddUserAdmin,
   VerifyAdmin,
   UpdateAdmin,
-} = require("../controller/Admin_PanelController")
-const express = require("express")
+} = require("../controller/Admin_PanelController");
+const express = require("express");
 
-const route = express.Router()
+const route = express.Router();
 
 route.post("/AddAdmin", async (req, res) => {
-  const { Username, MasterKey } = req.body
+  const { Username, MasterKey } = req.body;
   if (Username && MasterKey) {
     try {
-      const result = await AddUserAdmin(Username, MasterKey)
-      res.status(200).json(result)
+      const result = await AddUserAdmin(Username, MasterKey);
+      res.status(200).json(result);
     } catch (error) {
-      res.status(400).json(error)
+      res.status(400).json(error);
     }
   } else {
-    res.status(404).json("Error while parsing")
+    res.status(404).json("Error while parsing");
   }
-})
+});
 
 route.post("/UpdateAdmin", async (req, res) => {
-  const { Token, Username } = req.body
+  const { Token, Username } = req.body;
   if (Token) {
     try {
-      const result = await UpdateAdmin(Token, Username)
+      const result = await UpdateAdmin(Token, Username);
       if (result) {
-        res.status(200).json({ Message: "Successfully Updated the Admin" })
+        res.status(200).json({ Message: "Successfully Updated the Admin" });
       }
     } catch (error) {
-      res.status(400).json({ Message: "User Already Exists" })
+      res.status(400).json({ Message: "User Already Exists" });
     }
   } else {
-    res.status(404).json({ Message: "Invalid credentials" })
+    res.status(404).json({ Message: "Invalid credentials" });
   }
-})
+});
 
 route.post("/VerifyAdmin", async (req, res) => {
-  const { Username, MasterKey } = req.body
+  const { Username, MasterKey } = req.body;
 
   if (Username && MasterKey) {
     try {
       VerifyAdmin(Username, MasterKey).then((result) => {
         if (result) {
-          const token = TokenGenerator.adminTokenGenerator(Username)
+          const token = TokenGenerator.adminTokenGenerator(Username);
           res.cookie("jwt", token, {
             maxAge: 24 * 60 * 60 * 1000, // for 1 day
-          })
+          });
           // console.log(token);
           // console.log(result)
-          res.status(200).json(result)
+          res.status(200).json(result);
         } else {
-          res.status(404).json(result)
+          res.status(404).json(result);
         }
-      })
+      });
     } catch (error) {
-      console.log(error)
-      res.status(400).json({ Message: "User Not Found" })
+      console.log(error);
+      res.status(400).json({ Message: "User Not Found" });
     }
   } else {
-    res.status(404).json({ Message: "Invalid credentials" })
+    res.status(404).json({ Message: "Invalid credentials" });
   }
-})
+});
 
-module.exports = route
+module.exports = route;
