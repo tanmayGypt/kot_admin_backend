6fc22512f7c29ae47e17c84769938b555916aa78
tanmayGypt <tanmay631@gmail.com>
2024-05-26 23:52:54 +0530

    api fixes

--- Routes/GuestsRoute.js
+++ Routes/GuestsRoute.js
@@ -1,15 +1,15 @@
-const { guestTokenGenerator } = require("../TokenGenerator")
-const auth = require("../auth")
+const { guestTokenGenerator } = require("../TokenGenerator");
+const auth = require("../auth");
 const {
   AddGuest,
   VerifyGuest,
   FetchAllGuests,
   FetchGuestById,
   DeleteGuestById,
-} = require("../controller/GuestsController")
-const express = require("express")
+} = require("../controller/GuestsController");
+const express = require("express");
 
-const route = express.Router()
+const route = express.Router();
 
 route.post("/AddGuest", auth, (req, res) => {
   const {
@@ -21,7 +21,7 @@ route.post("/AddGuest", auth, (req, res) => {
     IdentityType,
     IdentityNumber_Hashed,
     MobileNumber,
-  } = req.body
+  } = req.body;
   AddGuest(
     RoomNumber,
     RoomId,
@@ -33,63 +33,63 @@ route.post("/AddGuest", auth, (req, res) => {
     MobileNumber
   )
     .then((Result) => {
-      res.status(200).json(Result)
+      res.status(200).json(Result);
     })
     .catch((err) => {
-      res.status(404).json(err)
-    })
-})
+      res.status(404).json(err);
+    });
+});
 
 route.post("/VerifyGuest", async (req, res) => {
-  const EncodedRoomNo = req.query.roomId
-  const { MobileNumber } = req.body
+  const EncodedRoomNo = req.query.roomId;
+  const { MobileNumber } = req.body;
 
   try {
-    const item = await VerifyGuest(MobileNumber, EncodedRoomNo)
+    const item = await VerifyGuest(MobileNumber, EncodedRoomNo);
     if (item) {
-      const token = guestTokenGenerator(EncodedRoomNo, MobileNumber)
+      const token = guestTokenGenerator(EncodedRoomNo, MobileNumber);
       res.cookie("jwt", token, {
         maxAge: 30 * 60 * 1000, // 30 min
-      })
-      res.send(item)
+      });
+      res.send(item);
     } else {
-      console.log("not found")
-      res.status(400).json("Invalid Credentials")
+      console.log("not found");
+      res.status(400).json(item);
     }
   } catch (Err) {
-    res.status(400).json("Error While Verifying")
+    res.status(400).json("Error While Verifying");
   }
-})
+});
 
 route.get("/FetchAllGuests", auth, (req, res) => {
   FetchAllGuests()
     .then((Items) => {
-      res.status(200).send(Items)
+      res.status(200).send(Items);
     })
     .catch((err) => {
-      res.status(404).json(err)
-    })
-})
+      res.status(404).json(err);
+    });
+});
 route.get("/FetchGuestById/:RoomId", auth, (req, res) => {
-  const RoomId = req.params.RoomId
+  const RoomId = req.params.RoomId;
   FetchGuestById(RoomId)
     .then((Items) => {
-      res.status(200).send(Items)
+      res.status(200).send(Items);
     })
     .catch((err) => {
-      res.status(404).json(err)
-    })
-})
+      res.status(404).json(err);
+    });
+});
 
 route.get("/DeleteGuestById/:RoomId", auth, (req, res) => {
-  const RoomId = req.params.RoomId
+  const RoomId = req.params.RoomId;
   DeleteGuestById(RoomId)
     .then((Items) => {
-      res.status(200).send(Items)
+      res.status(200).send(Items);
     })
     .catch((err) => {
-      res.status(404).json(err)
-    })
-})
+      res.status(404).json(err);
+    });
+});
 
-module.exports = route
+module.exports = route;
