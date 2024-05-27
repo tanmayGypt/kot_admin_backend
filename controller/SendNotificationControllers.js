6b213d1ec586556d7950e574b5a760d2b7f6abed
Rakesh <krakesh7788@gmail.com>
2024-05-27 16:02:00 +0530

    unsubscribe to topic added

--- controller/SendNotificationControllers.js
+++ controller/SendNotificationControllers.js
@@ -1,9 +1,10 @@
-const { admin } = require("../firebase");
-const { Admin_Panel } = require("../models");
+const { admin } = require("../firebase")
+const { Admin_Panel } = require("../models")
+
+const topic = "KOT_Orders"
 
 async function sendNotification(orderId, roomNumber) {
-  const topic = "KOT_Orders";
-  console.log("/send-notification");
+  console.log("/send-notification")
 
   const message = {
     notification: {
@@ -11,30 +12,41 @@ async function sendNotification(orderId, roomNumber) {
       body: `${roomNumber}`,
     },
     topic: topic,
-  };
+  }
 
   const response = admin
     .messaging()
     .send(message)
     .then((response) => {
-      console.log("Successfully sent message:", response);
+      console.log("Successfully sent message:", response)
     })
     .catch((error) => {
-      console.log("Error sending message:", error);
-    });
+      console.log("Error sending message:", error)
+    })
 }
 
 async function subscribeToTopic(token) {
   const response = admin
     .messaging()
-    .subscribeToTopic(token, "KOT_Orders")
+    .subscribeToTopic(token, topic)
     .then((response) => {
-      console.log("Successfully subscribed to topic:", response);
-      return;
+      console.log("Successfully subscribed to topic:", response)
+      return
     })
     .catch((error) => {
-      console.log("Error subscribing to topic:", error);
-    });
+      console.log("Error subscribing to topic:", error)
+    })
+}
+async function unsubscribeToTopic(token) {
+  admin
+    .messaging()
+    .unsubscribeFromTopic(token, topic)
+    .then((response) => {
+      console.log("Successfully unsubscribed from topic:", response)
+    })
+    .catch((error) => {
+      console.log("Error unsubscribing from topic:", error)
+    })
 }
 
-module.exports = { sendNotification, subscribeToTopic };
+module.exports = { sendNotification, subscribeToTopic, unsubscribeToTopic }
